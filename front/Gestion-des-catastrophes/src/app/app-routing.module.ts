f2d6977628516c44646835690c5699d39b66b012
KhaledKhm <47531479+KhaledKhm@users.noreply.github.com>
2022-07-19 21:18:48 +0100

    updates

--- front/Gestion-des-catastrophes/src/app/app-routing.module.ts
+++ front/Gestion-des-catastrophes/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home',  component: HomeComponent },
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path:'sidebar',component:SidebarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
