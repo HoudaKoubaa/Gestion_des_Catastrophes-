e02bc0bcbee5978f88eedc2878e8777c9f98f6d3
KhaledKhm <47531479+KhaledKhm@users.noreply.github.com>
2022-07-20 14:33:52 +0100

    testing

--- front/Gestion-des-catastrophes/src/app/app-routing.module.ts
+++ front/Gestion-des-catastrophes/src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import { HomeComponent } from './components/home/home.component';
 import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
 
 const routes: Routes = [
-  {path: 'home',  component: HomeComponent },
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path:'sidebar',component:SidebarComponent},
+  {path: 'home',  component: HomeComponent },
+ 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- front/Gestion-des-catastrophes/src/app/components/shared/sidebar/sidebar.component.ts
+++ front/Gestion-des-catastrophes/src/app/components/shared/sidebar/sidebar.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
+    console.log("Pot");
   }
 
 }
