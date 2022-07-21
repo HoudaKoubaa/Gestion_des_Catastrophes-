6675f581101c42b3f676ec70b668c81539285f62
KhaledKhm <47531479+KhaledKhm@users.noreply.github.com>
2022-07-21 14:04:32 +0100

    updates

--- front/Gestion-des-catastrophes/src/app/app.module.ts
+++ front/Gestion-des-catastrophes/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgbModal, NgbModalModule, NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { BodyComponent } from './components/shared/body/body.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,6 +21,10 @@ import { BodyComponent } from './components/shared/body/body.component';
     BodyComponent
   ],
   imports: [
+    RouterModule,
+    HttpClientModule,
+    NgbModalModule,
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
