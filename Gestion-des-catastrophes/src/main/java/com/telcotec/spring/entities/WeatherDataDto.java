28620b57c39e82934605ab1c6a777d5436887a75
KhaledKhm <47531479+KhaledKhm@users.noreply.github.com>
2022-08-15 10:37:13 +0100

    up

---
+++ Gestion-des-catastrophes/src/main/java/com/telcotec/spring/entities/WeatherDataDto.java
@@ -0,0 +1,16 @@
+package com.telcotec.spring.entities;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PUBLIC)
+public class WeatherDataDto {
+
+		
+}
