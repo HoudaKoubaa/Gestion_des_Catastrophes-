77785a80193c17c4d57ca83c60b1828279ad0f71
KhaledKhm <47531479+KhaledKhm@users.noreply.github.com>
2022-08-24 12:57:09 +0100

    updates

--- front/Gestion-des-catastrophes/src/app/app-routing.module.ts
+++ front/Gestion-des-catastrophes/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { ListProgrammeComponent } from './components/programme/list-programme/list-programme.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { NotFoundComponent } from './components/shared/not-found/not-found.component';
 import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
@@ -10,8 +11,7 @@ const routes: Routes = [
   {path: 'home',  component: HomeComponent },
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'login',  component: LoginComponent},
-  {path:'sidebar',component:SidebarComponent},
-  {path:'footer',component:FooterComponent},
+  {path:'programme',component:ListProgrammeComponent},
   {path:'404',component:NotFoundComponent},
   {path:'notfound',component:NotFoundComponent},
   {path:'**',component:NotFoundComponent}
--- front/Gestion-des-catastrophes/src/app/components/programme/list-programme/list-programme.component.ts
+++ front/Gestion-des-catastrophes/src/app/components/programme/list-programme/list-programme.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Programme } from 'src/app/models/programme';
+import { ProgrammeService } from 'src/app/services/programme.service';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-list-programme',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-programme.component.css']
 })
 export class ListProgrammeComponent implements OnInit {
+  listProgrammes:any; 
+  form:boolean=false;
+   programme!:Programme;
+   closeResult!:string;
 
-  constructor() { }
+  constructor(private programmeService:ProgrammeService, private modalService: NgbModal) { }
 
   ngOnInit(): void {
   }
+
+  getAllProgrammes(){
+    this.programmeService.retrieveAllProgrammes().subscribe(res=>this.listProgrammes = res);
+  }
+
+  deleteProgramme(idProgramme:any){
+    this.programmeService.deleteProgramme(idProgramme).subscribe(()=>this.getAllProgrammes());
+  }
+
+  open(content: any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+    }
+    private getDismissReason(reason: any): string {
+      if (reason === ModalDismissReasons.ESC) {
+        return 'by pressing ESC';
+      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+        return 'by clicking on a backdrop';
+      } else {
+        return  `with: ${reason}`;
+      }
+    }
+    closeForm(){
+  
+    }
+    cancel(){
+      this.form = false;
+    }
+  
 
 }
